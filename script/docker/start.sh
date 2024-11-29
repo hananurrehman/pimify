205945285b4f0019874bcea6f8f6e939d0377b99
Suriya <suriyag1999@gmail.com>
2024-11-29 22:26:00 +0530

    minor change in dockerfile and scripts

--- script/docker/start.sh
+++ script/docker/start.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-python manage.py migrate
-python manage.py makemigrations api
-python manage.py migrate
-
 python manage.py collectstatic --noinput
 
-mkdir -p data
-mkdir -p media
-
-echo '+++ Creating super user... +++'
-DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=1qaz2wsx python manage.py createsuperuser --email=admin@admin.com --noinput
-python manage.py migrate
+echo "Superuser created with username 'admin' and password 'admin123'"
+echo "**IMPORTANT:** Change the password immediately after logging in for the first time."
 
 gunicorn core.wsgi:application -c /etc/gunicorn/gunicorn.conf.py
