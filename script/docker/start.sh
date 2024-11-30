4e277013ff2cb4402046698da3c0ab900b0a41e8
Hanan <16245934+hananurrehman@users.noreply.github.com>
2024-11-30 01:52:55 +0100

    feat: Playwright setup and first test

--- script/docker/start.sh
+++ script/docker/start.sh
@@ -9,6 +9,8 @@ python manage.py collectstatic --noinput
 mkdir -p data
 mkdir -p media
 
-python manage.py createsuperuser
+echo '+++ Creating super user... +++'
+DJANGO_SUPERUSER_USERNAME=admin DJANGO_SUPERUSER_PASSWORD=1qaz2wsx python manage.py createsuperuser --email=admin@admin.com --noinput
+python manage.py migrate
 
 gunicorn core.wsgi:application -c /etc/gunicorn/gunicorn.conf.py
