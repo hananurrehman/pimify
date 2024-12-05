e1d40e8b3b07874e05789705478da281b553f314
Hanan <16245934+hananurrehman@users.noreply.github.com>
2024-12-06 00:58:44 +0100

    Basic playwright setup

---
+++ tests/auth.setup.ts
@@ -0,0 +1,15 @@
+import { test as setup, expect } from '@playwright/test';
+import path from 'path';
+
+const authFile = path.join(__dirname, './auth/user.json');
+const username = process.env.DJANGO_SUPERUSER_USERNAME ?? ''
+const password = process.env.DJANGO_SUPERUSER_PASSWORD ?? ''
+
+setup('Login admin user', async ({ page }) => {
+    await page.goto('');
+    await page.locator('#id_username').fill(username)
+    await page.locator('#id_password').fill(password)
+    await page.getByRole('button', { name: 'Log In' }).click()
+    await expect(page.getByRole('link', { name: 'Dashboard' })).toBeVisible()
+    await page.context().storageState({ path: authFile });
+});
